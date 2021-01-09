b63dbb5e951d381bb1e14fdd50c65185dc0566ef
the96 <66038157+godcantpee@users.noreply.github.com>
2021-01-09 15:51:44 -0800

    Added everything previously

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# forum-post-hub
-Allows multiple users to register and log in, and post what text they want to the front page.
+# CSC 317 Term Project
+
+## Purpose
+
+The purpose of this repository is to store all the code for your web application. This also includes the history of all commits made and who made them. Only code submitted on the master branch will be graded.
+
+Please follow the instructions below and fill in the information requested when prompted.
+
+## Student Information
+
+|               | Information   |
+|:-------------:|:-------------:|
+| Student Name  | Thomas He    |
+| Student ID    | 920894114       |
+| Student Email | the2@mail.sfsu.edu    |
+
+
+
+# Build/Run Instructions
+
+
+***The main build is on branch 1, not master, I couldn't merge 1 onto master.***
+## Build Instructions
+1. all packages have been installed
+
+## Run Instructions
+1. open terminal/command prompt
+2. cd to the cis317-termproject folder
+3. type .\final\env\Scripts\activate.bat  to the terminal/command prompt
+3. type py .\final\run.py to the command prompt
+4. The webdomain is localhost:5000
---
+++ final/final/routes.py
@@ -0,0 +1,115 @@
+import os
+import secrets
+from PIL import Image
+from flask import render_template, url_for, flash, redirect, request, Flask
+from final import app, db, bcrypt
+from final.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from final.classes import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = { 'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/")
+@app.route("/home")
+def home():
+    db.create_all
+    posts = Post.query.all()
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/pictures', picture_fn)
+
+    output_size = (300, 300)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+@app.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    return render_template('account.html', title='Account', form=form)
+
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    pic = ''
+    if form.validate_on_submit():
+        if form.picture.data:
+            pic = save_picture(form.picture.data)
+
+        post = Post(title=form.title.data, content=form.content.data, author=current_user, image_file=pic)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='Create a New Post', form=form, legend='New Post')
+
+
